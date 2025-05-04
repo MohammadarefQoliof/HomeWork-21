409f64901272eb9a1581a7b53350c4a4098bfe4d
MohammadarefQoliof <mhmmdarifquliyev33@gmail.com>
2025-05-04 23:11:19 +0400

    HomeWork 21

---
+++ HomeWork21.js
@@ -0,0 +1,14 @@
+// Task 1
+function greeting(name){
+    console.log(`${name} is a JS developer`);
+}
+greeting("Məhəmmədarif")
+
+// Task 2
+function user(userName){
+    console.log(`${userName} is active now`);
+}
+
+user("Vaqif")
+user("Kamran")
+user("Kənan")
